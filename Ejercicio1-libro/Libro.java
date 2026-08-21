bc39eec66a54f1aa0139a0d6289c23cc2714f421
RobertoMartinezDiaz <rmartinezd@unicartagena.edu.co>
2026-08-21 16:05:56 -0500

    feat(Libro): añade el constructor por defecto con valores predeterminados

---
+++ Ejercicio1-libro/Libro.java
@@ -0,0 +1,16 @@
+public class Libro {
+    String titulo;
+    String autor;
+    int numeroPaginas;
+
+    public Libro(){
+
+        titulo = "Cien años de soledad";
+        autor = "Gabriel García Marques";
+        numeroPaginas = 496
+
+    }
+
+
+
+}
