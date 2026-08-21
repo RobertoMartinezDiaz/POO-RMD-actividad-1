71f7b58426c744040a02dbb94c47d8ea3ca55de0
RobertoMartinezDiaz <rmartinezd@unicartagena.edu.co>
2026-08-21 16:24:02 -0500

    feat(Libro): añade el constructor parametrizado

--- Ejercicio1-libro/Libro.java
+++ Ejercicio1-libro/Libro.java
@@ -7,7 +7,14 @@ public class Libro {
 
         titulo = "Cien años de soledad";
         autor = "Gabriel García Marques";
-        numeroPaginas = 496
+        numeroPaginas = 496;
+
+    }
+
+    public Libro (String autor, String titulo, int numeroPaginas){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.numeroPaginas = numeroPaginas;
 
     }
 
