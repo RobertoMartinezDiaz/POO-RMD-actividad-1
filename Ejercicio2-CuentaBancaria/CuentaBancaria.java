933c4f931f036b87b6fc4250c175b14e18f8dda7
RobertoMartinezDiaz <rmartinezd@unicartagena.edu.co>
2026-08-21 17:02:01 -0500

    feat(CuentaBancaria): añade el constructor por defecto

---
+++ Ejercicio2-CuentaBancaria/CuentaBancaria.java
@@ -0,0 +1,13 @@
+public class cuentaBancaria {
+    int numeroCuenta;
+    double saldo;
+    String tipoCuenta;
+
+
+    public CuentaBancaria(){
+        numeroCuenta = 00000000;
+        saldo 0.0;
+        tipoCuenta = "Ahorros";
+
+    }
+}
