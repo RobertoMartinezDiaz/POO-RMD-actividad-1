b62f0446b41928f6329d3010705c477c1e735a17
RobertoMartinezDiaz <rmartinezd@unicartagena.edu.co>
2026-08-22 03:38:38 -0500

    feat(Estudiante): añade el constructor por defecto con valores predeterminados

---
+++ Ejercicio3-Estudiante/Estudiante.java
@@ -0,0 +1,12 @@
+public class Estudiante {
+    String nombre;
+    int edad;
+    String curso;
+
+    public Estudiante(){
+        nombre = "Roberto Martinez Diaz";
+        edad = 16;
+        curso = "Noveno 2";
+    }
+
+}
